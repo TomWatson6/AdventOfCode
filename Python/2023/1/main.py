af07a854c476e2d35e4511ca83b1b86cce46d2a9
Tom Watson <tom.watson6@outlook.com>
2023-12-01 09:52:52 +0000

    add solution for day 1 2023

---
+++ Python/2023/1/main.py
@@ -0,0 +1,63 @@
+import re
+
+with open("input.txt") as f:
+    lines = [x.strip() for x in f.readlines()]
+
+C = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
+}
+
+def convert(i):
+    if i in C:
+        return C[i]
+
+    return i
+
+total = 0
+
+def replace_nums(s, p2):
+    if p2:
+        for num, digit in C.items():
+            s = s.replace(num, f"{num[0]}{digit}{num[-1]}")
+
+    return s
+
+for line in lines:
+    line = replace_nums(line, False)
+    matches = re.findall(r"\d", line)
+    total += int(matches[0] + matches[-1])
+
+print("Part 1:", total)
+
+total = 0
+
+for line in lines:
+    line = replace_nums(line, True)
+    matches = re.findall(r"\d", line)
+    total += int(matches[0] + matches[-1])
+
+print("Part 2:", total)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
