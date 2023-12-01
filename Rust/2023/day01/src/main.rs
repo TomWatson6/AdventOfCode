062c23de936b4e3fba2c9ecca89383865f2f27f1
Tom Watson <tom.watson6@outlook.com>
2023-12-01 12:26:36 +0000

    add rust solution for day 1 2023

--- Rust/2023/day01/src/main.rs
+++ Rust/2023/day01/src/main.rs
@@ -14,7 +14,60 @@ fn read_file(file_name: &str) -> String {
     contents
 }
 
-fn main() {
+fn convert(mappings: &Vec<(&str, &str)>, s: String) -> String {
+    let mut output = String::from(s);
+
+    for (text, rep) in mappings {
+        let replacement = format!("{}{}{}", text.chars().nth(0).unwrap(), rep, text.chars().nth(text.len() - 1).unwrap());
+        output = output.replace(text, &replacement);
+    }
+
+    output.to_string()
+}
+
+fn count_numbers(p2: bool) -> i32 {
+    let mappings = vec![
+        ("one", "1"),
+        ("two", "2"),
+        ("three", "3"),
+        ("four", "4"),
+        ("five", "5"),
+        ("six", "6"),
+        ("seven", "7"),
+        ("eight", "8"),
+        ("nine", "9"),
+    ];
+
     let input = read_file("input.txt");
+    let mut total = 0;
+
+    for mut line in input.lines() {
+        if p2 {
+            total += assemble_number(convert(&mappings, line.to_string()).as_str());
+        } else {
+            total += assemble_number(line);
+        }
+    }
+
+    total
+}
+
+fn assemble_number(line: &str) -> i32 {
+    let mut nums: Vec<i32> = Vec::new();
+
+    for c in line.chars() {
+        match c.to_string().parse::<i32>() {
+            Ok(x) => nums.push(x),
+            Err(_) => continue,
+        }
+    }
+
+    let combined = format!("{}{}", nums[0].to_string(), nums[nums.len()-1].to_string());
+    combined.parse::<i32>().unwrap()
+}
+
+fn main() {
+    println!("Part 1: {}", count_numbers(false));
+    println!("Part 2: {}", count_numbers(true));
 }
 
--- Rust/2023/day01/target/debug/deps/day01-b0a75e5ff72feee3.d
+++ Rust/2023/day01/target/debug/deps/day01-b0a75e5ff72feee3.d
@@ -1,5 +1,5 @@
-/home/tom/Rust/AdventOfCode/2023/day01/target/debug/deps/libday01-b0a75e5ff72feee3.rmeta: src/main.rs
+/home/tom/AdventOfCode/Rust/2023/day01/target/debug/deps/libday01-b0a75e5ff72feee3.rmeta: src/main.rs
 
-/home/tom/Rust/AdventOfCode/2023/day01/target/debug/deps/day01-b0a75e5ff72feee3.d: src/main.rs
+/home/tom/AdventOfCode/Rust/2023/day01/target/debug/deps/day01-b0a75e5ff72feee3.d: src/main.rs
 
 src/main.rs:
