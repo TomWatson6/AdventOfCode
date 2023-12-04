0b6183883d09b58e2316a93374c44bb3eb3c4765
Tom Watson <tom.watson6@outlook.com>
2023-12-04 14:57:31 +0000

    update rust solution to correct answer

--- Rust/2023/day04/src/main.rs
+++ Rust/2023/day04/src/main.rs
@@ -1,5 +1,10 @@
 #![allow(unused)]
 
+use peak_alloc::PeakAlloc;
+
+#[global_allocator]
+static PEAK_ALLOC: PeakAlloc = PeakAlloc;
+
 use std::io::{self, Read};
 use std::vec;
 use std::io::{Write, BufReader, BufRead, ErrorKind};
@@ -14,7 +19,7 @@ fn read_file(file_name: &str) -> String {
     contents
 }
 
-fn get_num_cards(cards: &HashMap<i32, (HashSet<i32>, HashSet<i32>)>, id: &i32, winning_cards: &HashMap<i32, i32>, memo: &mut HashMap<i32, i32>) -> i32 {
+fn get_num_cards(cards: &HashMap<usize, (HashSet<usize>, HashSet<usize>)>, id: &usize, winning_cards: &HashMap<usize, usize>, memo: &mut HashMap<usize, usize>) -> usize {
     if let Some(x) = memo.get(id) {
         return *x;
     }
@@ -39,8 +44,8 @@ fn get_num_cards(cards: &HashMap<i32, (HashSet<i32>, HashSet<i32>)>, id: &i32, w
 fn main() {
     let input = read_file("input.txt");
 
-    let mut cards: HashMap<i32, (HashSet<i32>, HashSet<i32>)> = HashMap::new();
-    let mut winning_cards: HashMap<i32, i32> = HashMap::new();
+    let mut cards: HashMap<usize, (HashSet<usize>, HashSet<usize>)> = HashMap::new();
+    let mut winning_cards: HashMap<usize, usize> = HashMap::new();
 
     for line in input.lines() {
         let parts: Vec<&str> = line.split(' ').filter(|x| !x.is_empty()).collect();
@@ -50,7 +55,7 @@ fn main() {
                 let mut ch = x.chars().collect::<Vec<char>>();
                 ch = ch[..ch.len() - 1].to_vec();
                 let id_str = ch.into_iter().collect::<String>();
-                match id_str.parse::<i32>() {
+                match id_str.parse::<usize>() {
                     Ok(x) => x,
                     Err(e) => panic!("{} -> {:?}", id_str, e),
                 }
@@ -58,8 +63,8 @@ fn main() {
             None => panic!("No ID found in line: {}", line),
         };
 
-        let mut winning: HashSet<i32> = HashSet::new();
-        let mut owned: HashSet<i32> = HashSet::new();
+        let mut winning: HashSet<usize> = HashSet::new();
+        let mut owned: HashSet<usize> = HashSet::new();
         let mut winners = true;
 
         for i in 2..parts.len() {
@@ -68,7 +73,7 @@ fn main() {
                 continue
             }
 
-            match parts[i].parse::<i32>() {
+            match parts[i].parse::<usize>() {
                 Ok(x) => {
                     if winners {
                         winning.insert(x);
@@ -84,29 +89,33 @@ fn main() {
         *cards.entry(id).or_insert((HashSet::new(), HashSet::new())) = (winning, owned);
     }
 
-    let mut total = 0;
+    let mut total: usize = 0;
 
     for (id, sets) in cards.iter() {
-        let winners: HashSet<&i32> = sets.0.intersection(&sets.1).collect();
+        let winners: HashSet<&usize> = sets.0.intersection(&sets.1).collect();
 
         if winners.len() > 0 {
-            *winning_cards.entry(*id).or_insert(0) = winners.len() as i32;
+            *winning_cards.entry(*id).or_insert(0) = winners.len() as usize;
 
-            let base: i32 = 2;
-            let count = base.pow((winners.len() - 1) as u32);
+            let base: usize = 2;
+            let count: usize = base.pow((winners.len() - 1) as u32) as usize;
             total += count;
         }
     }
 
     println!("Part 1: {}", total);
 
-    total = 0;
-    let mut memo: HashMap<i32, i32> = HashMap::new();
+    total = cards.len();
+    let mut memo: HashMap<usize, usize> = HashMap::new();
 
     for (id, _) in cards.iter() {
         total += get_num_cards(&cards, &id, &winning_cards, &mut memo)
     }
     
     println!("Part 2: {}", total);
+
+
+    let peak_mem = PEAK_ALLOC.peak_usage_as_mb();
+    println!("The max amount that was used {}", peak_mem);
 }
 
